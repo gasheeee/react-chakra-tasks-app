6c3bf6db27a0e9c8d63becd504a6ce9da02cc034
gasheeee <higamasa1601@gmail.com>
2021-09-28 10:33:57 +0900

    環境構築完了

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { FC } from 'react';
+import Header from "./header";
 
-const App = () => {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Update the count (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 10000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
+      <header className="header">
+        <Header></Header>
       </header>
     </div>
   );
