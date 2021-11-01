696d5f0c0765b89837e57e508a86b7f2c6eeaf5c
gasheeee <higamasa1601@gmail.com>
2021-11-01 15:15:09 +0900

    OAuth2を使ったサインイン、サインアウト機能

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,11 +1,15 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, useRadio } from '@chakra-ui/react';
 import { Header } from '../molecules/Header';
 
 export const App: FC = () => {
+  //生成したgapi.auth2インスタンス
+  const [googleAuthInstance, setGoogleAuthInstance] = useState<gapi.auth2.GoogleAuth>();
+  //サインイン状態かどうか
   const [isSignedIn, setIsSignedIn] = useState<Boolean>(false);
 
   const initClient = () => {
+    //APIリクエストを行うために使用するgapi.client objectを初期化
     gapi.client
       .init({
         apiKey: import.meta.env.SNOWPACK_PUBLIC_API_KEY,
@@ -14,28 +18,40 @@ export const App: FC = () => {
         scope: import.meta.env.SCOPES,
       })
       .then(() => {
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignedInStatus);
-        updateSignedInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        setGoogleAuthInstance(gapi.auth2.getAuthInstance());
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  const updateSignedInStatus = (isSignedIn: Boolean) => {
+  const updateSignInStatus = (isSignedIn: Boolean) => {
     setIsSignedIn(isSignedIn);
   };
 
   const handleSignedIn = () => {
-    gapi.load('client:auth2', initClient);
+    googleAuthInstance?.signIn();
   };
+
+  const handleSignOut = () => {
+    googleAuthInstance?.signOut();
+  };
+
   useEffect(() => {
     if (!gapi) return;
-  }, [isSignedIn, gapi.load]);
+    //初期インスタンス生成
+    gapi.load('client:auth2', initClient);
+  }, [gapi]);
+
+  useEffect(() => {
+    if (!googleAuthInstance) return;
+    googleAuthInstance.isSignedIn.listen(updateSignInStatus);
+    updateSignInStatus(googleAuthInstance.isSignedIn.get());
+  }, [googleAuthInstance, isSignedIn]);
 
   return (
     <Box>
-      <Header isSignedIn={isSignedIn} handleSignedIn={handleSignedIn}></Header>
+      <Header isSignedIn={isSignedIn} onAuthorizationClick={isSignedIn ? handleSignOut : handleSignedIn}></Header>
     </Box>
   );
 };
